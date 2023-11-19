77c0522df34268f1486485edef5046e2fc00dd2f
denissav203@gmail.com <denissav203@gmail.com>
2023-11-19 20:16:09 +0300

    Rework

--- Laba4/Main.java
+++ Laba4/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		LogsFile.StartLog("Laba4/logs.txt");
 		Integer numberOfObjects = 10;
@@ -13,8 +13,8 @@ public class Main {
 		GenerateArrayList.FillRandomList(vehicleList, numberOfObjects);
 
 		ArrayList<Vehicle> vehicleListCapacity = new ArrayList<>();
-		GenerateArrayList.FillRandomList(vehicleListCapacity, (numberOfObjects * 3)/2 + 1);
-		
+		GenerateArrayList.FillRandomList(vehicleListCapacity, (numberOfObjects * 3) / 2 + 1);
+
 		LinkedList<Vehicle> vehicleLinked = new LinkedList<>();
 		GenerateArrayList.FillRandomLinked(vehicleLinked, numberOfObjects);
 		LogsFile.EndLog("Laba4/logs.txt");
